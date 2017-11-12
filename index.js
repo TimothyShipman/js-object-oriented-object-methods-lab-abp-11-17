335df6c9402b2eebae2f9d02632b83f79ce450a8
Tim Shipman <bricklayertim@yahoo.com>
2017-11-12 21:41:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ class BoardMember {
 
     releasePressStatement(){return `You will see great things from Scuber.`}
 
+  sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
+
 }
