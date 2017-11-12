5435df3107318edc04b366079d97907ad06ea788
Tim Shipman <bricklayertim@yahoo.com>
2017-11-12 21:27:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-function BoardMember (name, homeState, training) {
+class BoardMember {constructor(name, homeState, training) 
     this.name = name
     this.homeState = homeState
     this.training = training
